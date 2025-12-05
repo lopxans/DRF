4367f6e432826c59130846f8e12737546a0dd16a
lopxans <lopxans@gmail.com>
2025-12-05 15:26:23 +0545

    Function Based API View For Brower

---
+++ P8/api/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework import status
+
+
+# @api_view(['GET']) # or @api_view()
+# def hello_world(request):
+#     return Response({'msg': 'This is Get Request'})
+
+@api_view(['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
+def student_api(request, pk=None):
+
+    """ Get Data """
+    if request.method == 'GET':
+        if pk is not None:
+            stu = Student.objects.get(id=pk)
+            serializer = StudentSerializer(stu)
+            return Response(serializer.data)
+        stu = Student.objects.all()
+        serializer = StudentSerializer(stu, many=True)
+        return Response(serializer.data)
+
+    """ Create Data """
+    if request.method == 'POST':
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg': 'Data Created'}, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    
+    """ Complete Update Data """
+    if request.method == 'PUT':
+        stu = Student.objects.get(id=pk)
+        serializer = StudentSerializer(stu, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg': 'Complete Data Updated'}, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors)
+
+    """ Partial Upade Data """
+    if request.method == 'PATCH':
+        stu = Student.objects.get(pk=pk)
+        serializer = StudentSerializer(stu, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg': 'Partial Data Updated'}, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors)
+
+    """ Delete Data """
+    if request.method == 'DELETE':
+        stu = Student.objects.get(pk=pk)
+        stu.delete()
+        
+        return Response({'msg': 'Data Updated'}, status=status.HTTP_201_CREATED)
