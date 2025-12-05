145e2b6cfbdbc3cc2329cf4b12a6acd6649b0436
lopxans <lopxans@gmail.com>
2025-12-05 14:51:43 +0545

    Function Based API View in DRF

---
+++ P7/api/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Student
+from .serializers import StudentSerializer
+
+
+# @api_view(['GET']) # or @api_view()
+# def hello_world(request):
+#     return Response({'msg': 'This is Get Request'})
+
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+def student_api(request):
+
+    """ Get Data """
+    if request.method == 'GET':
+        id = request.data.get('id')
+        if id is not None:
+            stu = Student.objects.get(id=id)
+            serializer = StudentSerializer(stu)
+            return Response(serializer.data)
+        stu = Student.objects.all()
+        serializer = StudentSerializer(stu, many=True)
+        return Response(serializer.data)
+
+    """ Create Data """
+    if request.method == 'POST':
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg': 'Data Created'})
+        
+        return Response(serializer.errors)
+
+    """ Update Data """
+    if request.method == 'PUT':
+        id = request.data.get('id')
+        stu = Student.objects.get(pk=id)
+        serializer = StudentSerializer(stu, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg': 'Data Updated'})
+        
+        return Response(serializer.errors)
+
+    """ Delete Data """
+    if request.method == 'DELETE':
+        id = request.data.get('id')
+        stu = Student.objects.get(pk=id)
+        stu.delete()
+        
+        return Response({'msg': 'Data Updated'})
