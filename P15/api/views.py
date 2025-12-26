763a703f1155ac20ccd2c86259aaaf939050095e
lopxans <lopxans@gmail.com>
2025-12-26 19:11:16 +0545

    Basic Authentication & Permission Class in DRG

---
+++ P15/api/views.py
@@ -0,0 +1,21 @@
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework import viewsets
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
+
+class StudentModelViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    authentication_classes = [BasicAuthentication]
+    # permission_classes = [IsAuthenticated]
+    permission_classes = [IsAdminUser]
+
+# class StudentModelViewSet2(viewsets.ModelViewSet):
+#     queryset = Student.objects.all()
+#     serializer_class = StudentSerializer
+#     permission_classes = [AllowAny]
+
+# class StudentModelViewSet3(viewsets.ModelViewSet):
+#     queryset = Student.objects.all()
+#     serializer_class = StudentSerializer
