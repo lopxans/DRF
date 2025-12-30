b3cd4694473362375a1a7c7324bbdd811486c929
lopxans <lopxans@gmail.com>
2025-12-30 10:17:49 +0545

    Token Generate in DRF

---
+++ P20/api/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from .views import *
+from rest_framework.routers import DefaultRouter
+from api.views import StudentModelViewSet
+from rest_framework.authtoken.views import obtain_auth_token
+
+router = DefaultRouter()
+router.register('students', StudentModelViewSet, basename='students')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('gettoken/', obtain_auth_token),
+]
